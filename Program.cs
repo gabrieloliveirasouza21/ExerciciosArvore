ee8c71930e607c07b9f71b07f2668d216c146376
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-13 20:35:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ExerciciosArvore.Trees;
+
+internal class Program {
+    private static void Main(string[] args) {
+        //BinaryTreeNode btna = new BinaryTreeNode("a");
+        //BinaryTreeNode btnb = new BinaryTreeNode("b");
+        //BinaryTreeNode btnc = new BinaryTreeNode("c");
+        //BinaryTreeNode btnd = new BinaryTreeNode("d");
+
+        //btna.left = btnb.ToString();
+        //btna.right = btnc.ToString();
+        //btnb.left = btnd.ToString();
+        //btnb.right = btna.ToString();
+
+            //    a
+            //   / \
+            //  b   c
+            // / \   \
+            //d   e   f
+
+
+        
+
+
+    }
+}
---
+++ Trees/BinaryTreeNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosArvore.Trees {
+    internal class BinaryTreeNode {
+
+        public string val;
+        public string left;
+        public string right;
+
+        public BinaryTreeNode(string val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+}
