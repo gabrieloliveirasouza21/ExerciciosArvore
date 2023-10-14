9675db6703791749b706e241b88df6a7ce1b84f3
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-13 21:11:15 -0300

    training...

--- Program.cs
+++ Program.cs
@@ -12,11 +12,26 @@ internal class Program {
         //btnb.left = btnd.ToString();
         //btnb.right = btna.ToString();
 
-            //    a
-            //   / \
-            //  b   c
-            // / \   \
-            //d   e   f
+        //    a
+        //   / \
+        //  b   c
+        // / \   \
+        //d   e   f
+
+
+        BinaryTreeNode a = new BinaryTreeNode("a");
+        BinaryTreeNode b = new BinaryTreeNode("b");
+        BinaryTreeNode c = new BinaryTreeNode("c");
+        BinaryTreeNode d = new BinaryTreeNode("d");
+        BinaryTreeNode e = new BinaryTreeNode("e");
+        BinaryTreeNode f = new BinaryTreeNode("f");
+
+        a.left = b.ToString();
+        a.right = c.ToString();
+        b.left = d.ToString();
+        b.right = e.ToString();
+        c.right = f.ToString();
+
 
 
         
